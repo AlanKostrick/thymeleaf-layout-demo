51b0000df974ea2675bb571d567df371b5a53fca
YourGitHubName <alan@wecancodeit.org>
2018-11-28 17:28:45 -0500

    sending demo

---
+++ src/test/java/org/wecancodeit/layoutdemo/LayoutDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.layoutdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LayoutDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
